aa238a0b4643be35e2239780fa4ececea07aa2ea
owsibowsi <49979155+owsibowsi@users.noreply.github.com>
2021-05-17 15:32:53 +0200

    ss

--- TravelCatalogue/Infrastructure/Repository.cs
+++ TravelCatalogue/Infrastructure/Repository.cs
@@ -13,7 +13,7 @@ namespace TravelCatalogue.Infrastructure
         static Repository()
         {
             var India = new BoxTips("India", "Chicken Tikka Masala", "Women should be ready to cover themselves in certain areas", "Scooters and busses are the most common form of transport");
-            var Japan = new BoxTips("India", "Chicken Tikka Masala", "Women should be ready to cover themselves in certain areas", "Scooters and busses are the most common form of transport");
+            var Japan = new BoxTips("Japan", "Chicken Tikka Masala", "Women should be ready to cover themselves in certain areas", "Scooters and busses are the most common form of transport");
 
         }
     }
