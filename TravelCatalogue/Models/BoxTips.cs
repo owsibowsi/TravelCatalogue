8b40c60b042d8cdbaae30bc8e101ddcdee0cca6f
owsibowsi <49979155+owsibowsi@users.noreply.github.com>
2021-05-17 14:42:26 +0200

    Partial View setup

---
+++ TravelCatalogue/Models/BoxTips.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCatalogue.Models
+{
+    public class BoxTips
+    {
+        public string Title { get; set; }
+        public string Food { get; set; }
+        public string Culture { get; set; }
+        public string Transport { get; set; }
+        public string ImageFileName { get; set; }
+    }
+
+    // Cons
+}
